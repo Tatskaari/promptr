77673068f251ad4e550c1181badd3095b25f006a
Tatskaari <jonfpoole@gmail.com>
2019-10-19 18:24:13 +0100

    env vars created for bash

--- argprovider/unix/provider.go
+++ argprovider/unix/provider.go
@@ -1,1 +1,33 @@
 package unix
+
+import (
+	"os"
+	"os/exec"
+	"strings"
+)
+
+type LinuxArgProvider struct {}
+
+func (o *LinuxArgProvider) Username() string {
+	return os.Getenv("USER")
+}
+
+func (o *LinuxArgProvider) Hostname() string {
+	cmd :=  exec.Command("hostname")
+
+	b, err := cmd.Output()
+	if err != nil {
+		panic(err)
+	}
+
+	out := string(b)
+	return strings.TrimSuffix(out, "\n")
+}
+
+func (o *LinuxArgProvider) Prompt() string {
+	return "$"
+}
+
+func (o *LinuxArgProvider) WorkingDirectory() string {
+	return os.Getenv("PWD")
+}
--- main.go
+++ main.go
@@ -1,43 +1,10 @@
 package main
 
 import (
-	"github.com/jessevdk/go-flags"
-	"log"
+	"promptr/argprovider/unix"
 	"promptr/prompt"
 )
 
-var options struct {
-	Username string `short:"u"`
-	Hostname string `short:"n"`
-	Prompt string `short:"p"`
-	WorkingDirectory string `short:"d"`
-}
-
-//TODO(jpoole): Make this retrieve these values from the command line or env vars
-type opts struct {}
-
-func (o *opts) Username() string {
-	return options.Username
-}
-
-func (o *opts) Hostname() string {
-	return options.Hostname
-}
-
-func (o *opts) Prompt() string {
-	return options.Prompt
-}
-
-func (o *opts) WorkingDirectory() string {
-	return options.WorkingDirectory
-}
-
 func main() {
-	var parser = flags.NewParser(&options, flags.Default)
-	_, err := parser.Parse()
-	if err != nil {
-		log.Fatalf("failed to parse flags: %v", err)
-	}
-
-	println(prompt.GeneratePrompt(new(opts)))
+	println(prompt.GeneratePrompt(new(unix.LinuxArgProvider)))
 }
